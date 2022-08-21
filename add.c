193652ad7bb131d9969d672b3b5d3f7a9fbeb4a6
Joshua Alonge <alongejoshua@gmail.com>
2022-08-20 17:09:31 -0700

    update on monty

--- add.c
+++ add.c
@@ -1,32 +1,33 @@
 #include "monty.h"
-/**
- * f_add - adds the top two elements of the stack.
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
-void f_add(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
-	int len = 0, aux;
 
-	h = *head;
-	while (h)
+/**
+ * get_add - function that adds the top two elements of the stack
+ *
+ * @stack: pointer to the top of the stack
+ * @line_number: where the line number appears
+ *
+ * Return:
+ * - Upon success, nothing
+ * - Upon failure, EXIT_FAILURE
+ */
+void get_add(stack_t **stack, unsigned int line_number)
+{
+	stack_t *first, *second;
+
+	if ((*stack == NULL) || ((*stack)->next == NULL))
 	{
-		h = h->next;
-		len++;
-	}
-	if (len < 2)
-	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		fclose(file);
+		free(*stack);
 		exit(EXIT_FAILURE);
 	}
-	h = *head;
-	aux = h->n + h->next->n;
-	h->next->n = aux;
-	*head = h->next;
-	free(h);
+	else
+	{
+		first = *stack;
+		second = first->next;
+		second->n += first->n;
+		*stack = second;
+		(*stack)->prev = NULL;
+		free(first);
+	}
 }
